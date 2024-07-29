246706083219ee460b80a635c3e8e5a3ec377c8b
Chong Yu Tze <arnoldchongyutze@gmail.com>
2024-07-29 11:13:53 +0800

    Add files via upload

---
+++ Q1a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "MMU Fee Structure 2022" << endl ;
+    cout << "Programme" << "   " << "Duration" << "   " << "Fee"   << endl;
+    cout << "Diploma"   << "   " << "  2 Years"  << "    " << "23000" << endl;
+    cout << "Degree"    << "   " << "   3 Years"  << "    " << "60000"  << endl;
+    return 0;
+}
+
